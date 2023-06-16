1c6aa1d1b0addb5310a01164dc65558f1374cea3
unknown <alex.herbert.tech@gmail.com>
2023-06-16 12:55:17 +1200

    Added code snippets and instructions on how to use the modules.

---
+++ file_system_template.js
@@ -0,0 +1,37 @@
+//Installation for FS module. Run this in the terminal.
+npm install file-system --save
+
+//Requirement line of code for FS
+const fs = require('fs');
+
+//Class Constructor for JSON formatting (This example is from a record management system I built, change as necersarry) This will be included in the user input logic to map inputs to specific fields.
+class PatientRecord {
+    constructor(id, name, gender, email, dob, phone, job) {
+      this.id = id;
+      this.name = name;
+      this.gender = gender;
+      this.email = email;
+      this.dob = dob;
+      this.phone = phone;
+      this.job = job;
+    }
+  }
+
+  // Functions for saving and loading data to/from JSON file. File directory must be the location of your JSON file, the current one is just an example.
+  function loadRecords() {
+    try {
+        const data = fs.readFileSync('src/records.json'); //File directory must be the location of your JSON file, the current one is just an example.
+        records = JSON.parse(data);
+    } catch (error) {
+        //Handles file read errors or empty file
+        console.error('Error loading records:', error.message);
+    }
+  }
+
+  function saveRecords() {
+    try {
+        fs.writeFileSync('src/records.json' , JSON.stringify(records)); //File directory must be the location of your JSON file, the current one is just an example.
+    } catch (error) {
+        console.error('Error saving records:', error.message);
+    }
+  }
